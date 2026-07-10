b22b635b1fd22452b47bd2f9b3fa2d39c63c2f21
Zehee <zehee@users.noreply.github.com>
2026-07-10 12:33:00 +0800

    chore: remove build-desktop.bat from git

--- build-desktop.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set VCToolsVersion=
-call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
-cd /d D:\workspace\Reasonix-Code\desktop
-npm run tauri build
